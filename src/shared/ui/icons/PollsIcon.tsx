318efde23a0644e2ffcea3316bb1115caddf8c71
Expant <anton532202@gmail.com>
2026-04-24 14:13:39 +0300

    feat: add display of the number of polls to the menu and change the menu design

---
+++ src/shared/ui/icons/PollsIcon.tsx
@@ -0,0 +1,9 @@
+export function PollsIcon() {
+  return (
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none">
+      <rect x="1" y="9" width="3" height="5" rx="1" fill="currentColor" />
+      <rect x="6" y="5" width="3" height="9" rx="1" fill="currentColor" />
+      <rect x="11" y="1" width="3" height="13" rx="1" fill="currentColor" />
+    </svg>
+  );
+}
